a85d7e4076926bffb681ffc1856af3df1e43922c
Puong206 <bagasrf206@gmail.com>
2024-03-26 17:44:57 +0700

    Menu Pilihan

--- Pertemuan Keempat/Pertemuan Keempat.cpp
+++ Pertemuan Keempat/Pertemuan Keempat.cpp
@@ -31,7 +31,24 @@ int pembagian(int a, int b)
     return a / b;
 }
 
+
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int pilihan;
+    do
+    {
+        cout << "Kalkulator Sederhana" << endl;
+        cout << "====================" << endl;
+        cout << "1. Penjumlahan" << endl;
+        cout << "2. Pengurangan" << endl;
+        cout << "3. Perkalian" << endl;
+        cout << "4. Pembagian" << endl;
+        cout << "5. Keluar" << endl;
+        cout << "====================" << endl;
+        cout << "Masukan Menu Pilihan : ";
+        cin >> pilihan;
+
+
+    }
 }
